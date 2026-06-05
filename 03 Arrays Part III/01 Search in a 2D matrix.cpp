79de463cb2ef35425a8e374111a57b5d3001472a
Rajesh Jondhale <jondhaleraj2151@gmail.com>
2026-06-05 21:45:24 +0530

    Day 5

---
+++ 03 Arrays Part III/01 Search in a 2D matrix.cpp
@@ -0,0 +1,55 @@
+// You are given an m x n integer matrix matrix with the following two properties:
+
+// Each row is sorted in non-decreasing order.
+// The first integer of each row is greater than the last integer of the previous row.
+// Given an integer target, return true if target is in matrix or false otherwise.
+
+// You must write a solution in O(log(m * n)) time complexity.
+
+// Example 1:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -10^4 <= matrix[i][j], target <= 10^4
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int low = 0;
+        int high = (m * n) - 1;
+
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (matrix[mid / n][mid % n] == target)
+            {
+                return true;
+            }
+            else if (matrix[mid / n][mid % n] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
---
+++ 03 Arrays Part III/03 Majority Element-I.cpp
@@ -0,0 +1,61 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+
+// Input: nums = [3,2,3]
+// Output: 3
+// Example 2:
+
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 5 * 104
+// -109 <= nums[i] <= 109
+// The input is generated such that a majority element will exist in the array.
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int cnt = 0;
+        int el = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (cnt == 0)
+            {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (el == nums[i])
+            {
+                cnt++;
+            }
+            else
+            {
+                cnt--;
+            }
+        }
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (el == nums[i])
+            {
+                cnt1++;
+            }
+        }
+        if (cnt1 > (n / 2))
+        {
+            return el;
+        }
+        return -1;
+    }
+};
